aad310ce11d7ae249ae5fa63074a14a3630af036
Oliver Downard <olivermd@btinternet.com>
2015-10-08 13:09:37 +0100

    Added timer stuff

--- project1/main.c
+++ project1/main.c
@@ -1,14 +1,35 @@
 #include "lights.h"
 
-volatile unsigned int on = 0;
+volatile unsigned long on = 0;
 
 void SysTick_Handler(){
   on++;
+}
+void Delay(unsigned long tick){
+  unsigned long systickcnt;
+  systickcnt = on;
+  while ((on - systickcnt) < tick);
 }
 
 void main(void) {
   lightsInit();
   SysTick_Config(SystemCoreClock/1000 - 1);
-  //clearAll();
-  //displayNum(2);
+  clearAll();
+  Delay(500);
+  displayNum(2);
+  Delay(500);
+  displayNum(9);
+  int buzz = 0;
+  for(;;)    {
+    if(buzz == 0){
+      clearAll();
+      switchOnAll();
+      buzz=1;
+    } else {
+      buzz = 0;
+      clearAll();
+      displayNum(9);
+    }
+    Delay(500);
+  }
 }
