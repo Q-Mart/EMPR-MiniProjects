7e667d5c820c69057bb76472939cad74a4636724
Q-Mart <qumarthjash@gmail.com>
2015-10-15 12:12:54 +0100

    please work

--- project2/i2c_helper.c
+++ project2/i2c_helper.c
@@ -1,4 +1,4 @@
-#import "i2c_helper.h"
+#include "i2c_helper.h"
 void i2c_init(){
   PINSEL_CFG_Type pinCfg;
 
@@ -11,22 +11,26 @@ void i2c_init(){
   pinCfg.Pinnum = 28;
   PINSEL_ConfigPin(&pinCfg);
   I2C_Init(LPC_I2C0, 100000);
-  I2C_Cmd(LPC_I2C0, ENABLE)
+  I2C_Cmd(LPC_I2C0, ENABLE);
 }
 
-int 12c_deviceCount(){
+int i2c_deviceCount(){
   int addr;
+  int count = 0;
   uint8_t txbuff[8];
   uint8_t rxbuff[8];
   for(addr = 1; addr < 127; addr ++){
     I2C_M_SETUP_Type setupCfg;
     setupCfg.sl_addr7bit = addr;
-    tx_data = txbuff;
-    tx_length = 8;
-    rx_data = rxbuff;
-    rx_length = 8;
-    rx_count = 0;
-    I2C_MasterTransferData(LPC_I2C0, &setupCfg, I2C_TRANSFER_POLLING);
-    serialUsbWrite("Address Found")
+    setupCfg.tx_data = txbuff;
+    setupCfg.tx_length = 8;
+    setupCfg.rx_data = rxbuff;
+    setupCfg.rx_length = 8;
+    setupCfg.rx_count = 0;
+    if(I2C_MasterTransferData(LPC_I2C0, &setupCfg, I2C_TRANSFER_POLLING) == SUCCESS){
+      serialUSBWrite("Address Found");
+      count++;
+    }
   }
+  return count;
 }
