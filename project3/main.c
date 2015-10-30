4a9d66eb15fe14abc0d57c310c6ad029e68d7563
Oliver Downard <olivermd@btinternet.com>
2015-10-30 14:58:39 +0000

    Some ADC Stuff

---
+++ project3/main.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "stdio.h"
+
+int main(){
+  serialUSBInit();
+
+
+  PINSEL_CFG_Type pinCfg;
+
+  pinCfg.Funcnum = 1;
+  pinCfg.OpenDrain = 0;
+  pinCfg.Pinmode = 0;
+  pinCfg.Portnum = 0;
+  pinCfg.Pinnum = 23;
+  PINSEL_ConfigPin(&pinCfg);
+  ADC_Init(LPC_ADC, 200000);
+  ADC_IntConfig(LPC_ADC, 0, DISABLE);
+  ADC_ChannelCmd(LPC_ADC, 0, ENABLE);
+  serialUSBWrite("Is this working?");
+  char str[15];
+  int val;
+  while(1){
+    ADC_StartCmd(LPC_ADC, ADC_START_NOW);
+    while(!(ADC_ChannelGetStatus(LPC_ADC, 0, ADC_DATA_DONE)));
+    val = ADC_ChannelGetData(LPC_ADC, 0);
+    sprintf(str, "%d\n\r", val);
+    serialUSBWrite(str);
+  }
+}
---
+++ project3/main.h
@@ -0,0 +1,2 @@
+#include "serialUSB.h"
+#include "lpc17xx_adc.h"
